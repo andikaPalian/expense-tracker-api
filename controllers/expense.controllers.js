17e1441e33d4850bf47ff98ac7695d7edebfa753
Andika <andika12des@gmail.com>
2024-12-08 08:19:39 +0800

    second commit

--- controllers/expense.controllers.js
+++ controllers/expense.controllers.js
@@ -24,4 +24,87 @@ const addNewExpense = async (req, res) => {
     };
 };
 
-module.exports = {addNewExpense};
+// GET ALL EXPENSES
+const getAllExpenses = async (req, res) => {
+    try {
+        const {filter, category, startDate, endDate} = req.query;
+        const filters = {userId: req.user.id};
+        // Filter berdasarkan waktu
+        if (filter === "week") {
+            filters.date = {$gte: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)};
+        } else if (filter === "month") {
+            filters.date = {$gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)};
+        } else if (filter === "3months") {
+            filters.date = {$gte: new Date(Date.now() = 90 * 24 * 60 * 60 * 1000)};
+        } else if (startDate && endDate) {
+            // Jika user memberikan tanggal custom, gunakan sebagai filter
+            filters.date = {$gte: new Date(startDate), $lte: new Date(endDate)};
+        };
+        // Filter berdasarkan kategori
+        if (category) {
+            filters.category = category;
+        };
+        const expense = await Expense.find(filters);
+        res.status(200).json({expense});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal server error", error});
+    };
+};
+
+// GET SINGLE EXPENSE BY ID
+const getSingleExpense = async (req, res) => {
+    try {
+        const expense = await Expense.findById(req.params.id);
+        if (!expense) {
+            return res.status(404).json({message: "Expense not found"});
+        };
+        res.status(200).json({expense});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal server error", error});
+    };
+};
+
+// UPDATE EXPENSE
+const updateExpense = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({message: "Invalid expense id"});
+        };
+        const expense = await Expense.findById(req.params.id);
+        if (!expense) {
+            return res.status(404).json({message: 'Expense not found'});
+        };
+        if (expense.userId.toString() !== req.user.id) {
+            return res.status(403).json({message: "You are not authorized to update this expense"});
+        };
+        const updatedExpense = await Expense.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json({message: "Expense updated successfully", updatedExpense});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal server error", error});
+    };
+};
+
+// DELETE EXPENSE
+const deleteExpense = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({message: "Invalid expense id"});
+        };
+        const expense = await Expense.findByIdAndDelete(req.params.id);
+        if (!expense) {
+            return res.status(404).json({message: "Expense not found"});
+        };
+        if (expense.userId.toString() !== req.user.id) {
+            return res.status(403).json({message: "You are not authorized to delete this expense"});
+        };
+        res.status(200).json({message: "Expense deleted successfully"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal server error", error});
+    };
+};
+
+module.exports = {addNewExpense, getAllExpenses, getSingleExpense, updateExpense, deleteExpense};
--- routes/expense.routes.js
+++ routes/expense.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
-const { addNewExpense } = require("../controllers/expense.controllers");
+const { addNewExpense, getAllExpenses, getSingleExpense, updateExpense, deleteExpense } = require("../controllers/expense.controllers");
 const router = express.Router();
 
 router.use(authMiddleware);
 router.post("/add-expense", addNewExpense);
+router.get("/get-expense", getAllExpenses);
+router.get("/get-expense/:id", getSingleExpense);
+router.put("/update-expense/:id", updateExpense);
+router.delete("/delete-expense/:id", deleteExpense);
 
 module.exports = router;
